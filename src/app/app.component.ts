56ddf8f3fa134d2710ba88a87998a9f7bdb42502
abdel-nasser.adjibi <abdel-nasser.adjibi@adesso.ch>
2024-03-03 23:39:14 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html',
-
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
