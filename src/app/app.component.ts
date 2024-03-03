84e4991631cb5b4a2b3343f896a623990f44ecee
abdel-nasser.adjibi <abdel-nasser.adjibi@adesso.ch>
2024-03-03 19:09:16 +0100

    update content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
+
 })
 export class AppComponent {
   value = 'World';
